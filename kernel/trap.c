61a57236dd8e8e3899b1a5a5ccc3bc97db563c29
L1p23 <2977329229@qq.com>
2023-06-01 19:19:38 +0800

    lazyallocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,23 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if (r_scause() == 13 || r_scause() == 15) {
+    uint64 va = r_stval();
+    if (va >= p->sz || va < p->trapframe->sp)
+      p->killed = 1;
+    else {
+      char *pa = kalloc();
+      if (pa == 0)
+        p->killed = 1;
+      else {
+        memset(pa, 0, PGSIZE);
+        if (mappages(p->pagetable, PGROUNDDOWN(va), PGSIZE, (uint64)pa, PTE_W|PTE_R|PTE_U) != 0) {
+          kfree(pa);
+          p->killed = 1;
+        }
+      }
+    }
+    //printf("page fault at %p\n", va);
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
